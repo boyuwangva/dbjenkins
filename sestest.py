cd69db3b6e3dbc872de6107f09767364c340879a
EC2 Default User <ec2-user@ip-10-0-1-20.ap-northeast-1.compute.internal>
2017-12-07 07:06:17 +0000

    2nd test

--- sestest.py
+++ sestest.py
@@ -14,7 +14,7 @@ def sendEmail():
             },
             Message={
                 'Subject': {
-                    'Data': 'test 1207-1255',
+                    'Data': 'test 1207-1606',
                 },
                 'Body':{
                     'Text': {
